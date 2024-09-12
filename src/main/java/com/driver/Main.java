661c0d42b35d7c13a8b0e3c5b079a5cea51afe36
vijay-sankar-pamidimarri <sankerpamidimarri@gmail.com>
2024-09-12 23:01:19 +0530

    i comitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static  void main(String[] args){
+      RWOnly obj1=new RWOnly();
+      obj1.setter(3);
+      obj1.getter();
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+   private int data;
+//   private String val;
+   public int getter(){
+       return this.data;
+   }
+   public void setter(int val){
+       data=val;
+   }
 }
